d3bd6dbd86185d16022f57c34f11d895c0d02d86
diegothuran <diego.thuran@gmail.com>
2018-01-11 13:53:28 -0300

    Deploy files

--- cluster/views.py
+++ cluster/views.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 from rest_framework import generics
 from .models import Clusterizacao, Cluster, Review
-from .serializers import ClusterizacaoSerializer,ClusterSerializer,ReviewSerializer
+from .serializers import ClusterizacaoSerializer, ClusterSerializer, ReviewSerializer
 
 class ClusterizacaoList(generics.ListCreateAPIView):
     queryset = Clusterizacao.objects.all()
